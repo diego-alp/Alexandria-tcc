96ea3e26c290f9662bb9a11bb421dc16f484e55f
diego-alp <104037590+diego-alp@users.noreply.github.com>
2022-05-08 18:55:05 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="TCC-CSS\style(home).css">
+	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+	<title>Alexandria</title>
+
+</head>
+
+
+<body>
+	
+
+<h1 id='titulo'>ALEXANDRIA</h1>
+
+
+<div class="box">
+<i class='fas fa-book-open' style='font-size:75px;color:dodgerblue'></i>
+<h2>Sejam bem-vindos!!</h2>
+	<a href="login.php">Login</a>
+	<a href="formulario.php">Cadastre-se</a><br>
+	
+
+</div>
+
+</body>
+</html>
