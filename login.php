496f0c271f585a306b3aba501bddc9d83a4e0cf6
diego-alp <104037590+diego-alp@users.noreply.github.com>
2022-05-08 19:45:19 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="TCC-CSS\style(login-usuario).css">
+	<title>Tela de login</title>
+	<body background="Background-login-usuario.jpg"
+		background-attachment:fixed;
+		background-size:100%;
+	>
+
+		<style>
+			
+
+		</style>
+</head>
+</body>
+
+<div>
+<h1>Login</h1>
+<form action="testLogin.php" method="Post">
+	
+	<input type="tex" name="email" placeholder="email"> 
+	<br><br>
+    <input type="password" name="senha" placeholder="senha"> 
+    <br><br>
+    <input class="inputSubmit" type="submit" name="submit" value="Enviar">
+	<br><br>
+
+	<a href="home.php">Voltar</a>
+</form>
+</div>
+<body>
+
+</body>
+</html>
